b80a28ba1cdfd96f5200879b47570fc1ffd32151
fernandooliveira19 <fer.a.oliveira19@gmail.com>
2019-12-09 22:49:18 -0300

    initial commit

---
+++ src/main/java/com/fernando/oliveira/booking/service/impl/TravelerServiceImpl.java
@@ -0,0 +1,7 @@
+package com.fernando.oliveira.booking.service.impl;
+
+import com.fernando.oliveira.booking.service.TravelerService;
+
+public class TravelerServiceImpl implements TravelerService{
+
+}
