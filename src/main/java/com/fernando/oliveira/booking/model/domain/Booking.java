b14e0bfcf6a8b444ac79e49e92720658636b2832
fernandooliveira19 <fer.a.oliveira19@gmail.com>
2019-12-16 07:31:02 -0300

    correção mapeamento; configurações hibernate
    Signed-off-by: fernandooliveira19 <fer.a.oliveira19@gmail.com>

--- src/main/java/com/fernando/oliveira/booking/model/domain/Booking.java
+++ src/main/java/com/fernando/oliveira/booking/model/domain/Booking.java
@@ -12,6 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -71,7 +72,7 @@ public class Booking implements Serializable {
 	@Column(name="GUESTS")
 	private Integer guests;
 	
-	@OneToMany
+	@ManyToOne
 	@JoinColumn(name="TRAVELER_ID")
 	private Traveler traveler;
 	
