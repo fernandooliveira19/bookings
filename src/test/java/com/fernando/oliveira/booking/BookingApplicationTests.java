ac7247c9773a96f18ab9467528c09ab621eda4a4
fernandooliveira19 <fer.a.oliveira19@gmail.com>
2019-12-24 14:09:21 -0300

    classe de teste
    Signed-off-by: fernandooliveira19 <fer.a.oliveira19@gmail.com>

--- src/test/java/com/fernando/oliveira/booking/BookingApplicationTests.java
+++ src/test/java/com/fernando/oliveira/booking/BookingApplicationTests.java
@@ -1,8 +1,5 @@
 package com.fernando.oliveira.booking;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 //@SpringBootTest
 class BookingApplicationTests {
 
