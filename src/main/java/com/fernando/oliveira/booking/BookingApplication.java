5bab0469a75cedf14efab45edba9ea06ba33f30b
fernandooliveira19 <fer.a.oliveira19@gmail.com>
2020-02-27 09:18:01 -0300

    refactor imports
    Signed-off-by: fernandooliveira19 <fer.a.oliveira19@gmail.com>

--- src/main/java/com/fernando/oliveira/booking/BookingApplication.java
+++ src/main/java/com/fernando/oliveira/booking/BookingApplication.java
@@ -2,11 +2,6 @@ package com.fernando.oliveira.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.fernando.oliveira.booking.api.converter.Java8DateTimeConfiguration;
 
 @SpringBootApplication
 public class BookingApplication{
